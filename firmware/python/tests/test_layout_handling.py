c68eaf42a193307d3f3356a6d01351ceec124044
Kit Barnes <k.barnes@mhnltd.co.uk>
2021-03-25 20:42:18 +0000

    Add currently-unused-but-tested layout handling
    This is the better layer implementation. Still need to hook it up to the
    rest of the firmware.

---
+++ firmware/python/tests/test_layout_handling.py
@@ -0,0 +1,132 @@
+import pytest
+from therefore import layout, layout_handling
+from adafruit_hid.keycode import Keycode
+
+
+# TODO: dedicated test layout rather than just using my own layout
+
+def test_parse_layout():
+    layout_handling.parse_layout(layout.layout['layers'])
+
+
+class RecordingEmitters:
+    def __init__(self):
+        self.called_withs = []
+
+    def press(self, *args, **kwargs):
+        self.called_withs.append(('press', *args))
+
+    def release(self, *args, **kwargs):
+        self.called_withs.append(('release', *args))
+
+
+@pytest.fixture
+def recording_emitters():
+    return RecordingEmitters()
+
+
+@pytest.fixture(scope='session')
+def parsed_layout():
+    return layout_handling.parse_layout(layout.layout['layers'])
+
+
+@pytest.fixture
+def layout_handler(parsed_layout, recording_emitters):
+    return layout_handling.LayoutHandler(parsed_layout, recording_emitters.press, recording_emitters.release)
+
+
+_LPAREN = ('combo', {('key', Keycode.NINE), ('key', Keycode.SHIFT)})
+_PUNCFN = (6, 4)
+_FN = (6, 2)
+_LSHIFT = (0, 3)
+test_data = {
+    'vacuous': ([], []),
+    'press anything': (
+        [('press', (1, 0))],
+        [('press', ('key', 30))]),
+    'layer change emits nothing': (
+        [('press', (6, 4))],
+        []
+    ),
+    'punctuation layer release fn first': (
+        [('press', _PUNCFN), ('press', (11, 2)), ('release', _PUNCFN), ('release', (11, 2))],
+        [('press', _LPAREN), ('release', _LPAREN)]
+    ),
+    'punctuation layer release key first': (
+        [('press', _PUNCFN), ('press', (11, 2)), ('release', (11, 2)), ('release', _PUNCFN)],
+        [('press', _LPAREN), ('release', _LPAREN)]
+    ),
+    'fns layer combination': (
+        [
+            ('press', _LSHIFT),
+            ('press', _FN),
+            ('press', (10, 0)),
+            ('release', (10, 0)),
+            ('release', _LSHIFT),
+            ('release', _FN)
+        ],
+        [
+            ('press', ('key', Keycode.SHIFT)),
+            ('press', ('key', Keycode.F6)),
+            ('release', ('key', Keycode.F6)),
+            ('release', ('key', Keycode.SHIFT))
+        ]
+    ),
+    'switch command': (
+        [('press', (9, 0)), ('release', (9, 0))],
+        [('press', ('switch', [''])), ('release', ('switch', ['']))]
+    ),
+    'toggle layer': (
+        [
+            # Toggle FN
+            ('press', _PUNCFN),
+            ('press', _FN),
+            ('release', _FN),
+            ('release', _PUNCFN),
+
+            # Press F1
+            ('press', (1, 0)),
+            ('release', (1, 0)),
+
+            # Untoggle FN
+            ('press', _PUNCFN),
+            ('press', _FN),
+            ('release', _FN),
+            ('release', _PUNCFN),
+
+            # Press numeric 1
+            ('press', (1, 0)),
+            ('release', (1, 0)),
+        ],
+        [
+            ('press', ('key', Keycode.F1)),
+            ('release', ('key', Keycode.F1)),
+            ('press', ('key', Keycode.ONE)),
+            ('release', ('key', Keycode.ONE)),
+        ]
+    ),
+    'transparent passthrough': (
+        [
+            ('press', _PUNCFN),
+            # Press numeric 1
+            ('press', (1, 0)),
+            ('release', (1, 0)),
+            ('release', _PUNCFN),
+        ],
+        [
+            ('press', ('key', Keycode.ONE)),
+            ('release', ('key', Keycode.ONE)),
+        ]
+    )
+}
+
+
+@pytest.mark.parametrize("input_sequence,expected_output_sequence", test_data.values(), ids=test_data.keys())
+def test_example_layout(input_sequence, expected_output_sequence, layout_handler, recording_emitters):
+    for direction, (x, y) in input_sequence:
+        if direction[0] in ['u', 'r']:
+            layout_handler.release(x, y)
+        elif direction[0] in ['d', 'p']:
+            layout_handler.press(x, y)
+
+    assert recording_emitters.called_withs == expected_output_sequence
---
+++ firmware/python/therefore/layout_handling.py
@@ -0,0 +1,211 @@
+"""
+Parsing and "execution" of multi-layer layouts.
+"""
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+
+# TODO: per-country lookups
+lookups = {
+    ' ': 'spacebar',
+    '1': 'one',
+    '2': 'two',
+    '3': 'three',
+    '4': 'four',
+    '5': 'five',
+    '6': 'six',
+    '7': 'seven',
+    '8': 'eight',
+    '9': 'nine',
+    '0': 'zero',
+    '`': 'grave_accent',
+    '\'': 'quote',
+    ';': 'semicolon',
+    ',': 'comma',
+    '.': 'period',
+    '/': 'forward_slash',
+    '[': 'left_bracket',
+    ']': 'right_bracket',
+    '#': 'pound',
+    '~': {'shift', 'pound'},
+    '\\': 'keypad_backslash',
+    '=': 'equals',
+    '-': 'minus',
+    'ins': 'insert',
+    'del': 'delete',
+    'esc': 'escape',
+    'win': 'gui',
+    'left': 'left_arrow',
+    'right': 'right_arrow',
+    'up': 'up_arrow',
+    'down': 'down_arrow',
+    'caps': 'caps_lock',
+
+    # media controls
+    'prev_track': 'cc_scan_previous_track',
+    'next_track': 'cc_scan_next_track',
+    'playpause': 'cc_play_pause',
+    'louder': 'cc_volume_increment',
+    'softer': 'cc_volume_decrement',
+    'mute': 'cc_mute',
+
+    # shifted punctuation
+    '(': {'shift', 'nine'},
+    ')': {'shift', 'zero'},
+    '_': {'shift', '-'},
+    '^': {'shift', 'six'},
+    '&': {'shift', 'seven'},
+    '*': {'shift', 'eight'},
+}
+
+
+def parse_syskey(keystr):
+    # syskeys are things that interact with the keyboard itself
+    # e.g. changing modes, layers
+    keystr = keystr.replace(')', '')
+    command, _, argstr = keystr[1:].partition('(')
+    args = argstr.split(',')
+    return command, args
+
+
+def parse_consumer_control(keystr):
+    # media control buttons, basically
+    keystr = keystr[3:]  # strip the 'CC_'
+
+    # let the potential AttributeError through so typos are caught early
+    code = getattr(ConsumerControlCode, keystr.upper())
+
+    return 'consumer control', code
+
+
+def parse(keystr):
+    # check for defined aliases
+    keystr = lookups.get(keystr, keystr)
+
+    # list means type them in order
+    if isinstance(keystr, list):
+        return 'sequence', [parse(k) for k in keystr]
+
+    # set means treat one key as many (but press modifiers first)
+    if isinstance(keystr, set):
+        return 'combo', {parse(k) for k in keystr}
+
+    # check for system keys
+    if keystr[0] == ':':
+        # TODO: check that syskey commands are valid
+        return parse_syskey(keystr)
+
+    # check for consumer control keys
+    if keystr.lower().startswith('CC_'):
+        return parse_consumer_control(keystr)
+
+    # finally it must be a standard keycode
+    # again ignoring potential AttributeErrors so typos are caught early
+    return 'key', getattr(Keycode, keystr.upper())
+
+
+def parse_layout(layers_dict):
+    """ Read a text-based layout dict and return a similar dict of classes and numeric keycodes.
+
+     Verifies along the way.
+    """
+
+    def parse_layer(layer_definition):
+        return [[parse(keystr) for keystr in row] for row in layer_definition]
+
+    return {name: parse_layer(definition) for name, definition in layers_dict.items()}
+
+
+class LayerStack:
+    def __init__(self):
+        #                type         name
+        self._stack = [('permanent', 'default')]
+
+    def to(self, layer_name):
+        self._stack.insert(0, ('to', layer_name))
+
+    def un_to(self, layer_name):
+        self._stack.remove(('to', layer_name))
+
+    def toggle(self, layer_name):
+        entry = ('toggle', layer_name)
+        if entry in self._stack:
+            self._stack.remove(entry)
+        else:
+            self._stack.insert(0, ('toggle', layer_name))
+
+    @property
+    def enabled_layers(self):
+        return (layer_name for mode, layer_name in self._stack)
+
+    @property
+    def current(self):
+        return self._stack[0][1]
+
+
+LAYER_COMMANDS = {'to', 'toggle'}
+
+
+class LayoutHandler:
+    """ Takes physical key presses/releases and converts them into actions, taking layers into account.
+
+    May in future handle time passing while keys are pressed (e.g. QMK-style mod-tap keys or tap dance).
+    """
+
+    @classmethod
+    def parse(cls, layers_dict, *args, **kwargs):
+        return cls(parse_layout(layers_dict), *args, **kwargs)
+
+    def __init__(self, parsed_layout, emit_press_callable, emit_release_callable):
+        self.layers = parsed_layout
+
+        # actually more of a "which layers are enabled with what priority" maybe?
+        # fails to model a layer which has a toggle for itself (e.g. you can get it it temporarily with a :to(...) then
+        # toggle it permanently with a :toggle(...) then press that toggle again to turn it off
+        # TODO: handle temp-to-toggle layers
+        self.layer_stack = LayerStack()
+        self.as_pressed = {}
+
+        self.emit_press = emit_press_callable
+        self.emit_release = emit_release_callable
+
+    def lookup(self, x, y):
+        # TODO: check this performs adequately in the normal case
+        for layer_name in self.layer_stack.enabled_layers:
+            prospective = self.layers[layer_name][y][x]
+            if prospective[0] != 't':
+                return prospective
+
+    def press(self, x, y):
+        action = self.lookup(x, y)
+        self.as_pressed[x, y] = action
+
+        command, values = action
+
+        if command in LAYER_COMMANDS:
+            getattr(self, f'handle_{command}_pressed')(values)
+        else:
+            self.emit_press(action)
+
+    def release(self, x, y):
+        action = self.as_pressed.pop((x, y))
+        command, values = action
+
+        if command in LAYER_COMMANDS:
+            getattr(self, f'handle_{command}_released')(values)
+        else:
+            self.emit_release(action)
+
+    def handle_to_pressed(self, args):
+        layer = args[0]
+        self.layer_stack.to(layer)
+
+    def handle_to_released(self, args):
+        layer = args[0]
+        self.layer_stack.un_to(layer)
+
+    def handle_toggle_pressed(self, args):
+        pass  # we ignore the press and only react upon release
+
+    def handle_toggle_released(self, args):
+        layer = args[0]
+        self.layer_stack.toggle(layer)
